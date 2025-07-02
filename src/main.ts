ba29c7cf7ac19ba20ae50afb18514150f7f27966
seolman <tjfehdgns@gmail.com>
2025-07-02 10:37:11 +0900

    init chirpy

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import type { request } from "./types";
+
+async function handleRequests(reqs: Array<request>) {
+  for (const req of reqs) {
+    await handleRequest(req);
+  }
+}
+
+// don't touch below this line
+
+async function handleRequest(req: request) {
+  console.log(`Handling request for ${req.path}`);
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      console.log(`Done with request for ${req.path}`);
+      resolve();
+    }, 2000);
+  });
+}
+
+async function timeLimit() {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      console.log("5 seconds passed, killing server");
+      resolve();
+    }, 5000);
+  });
+}
+
+async function main() {
+  const requests: request[] = [];
+
+  for (let i = 0; i < 4; i++) {
+    requests.push({ path: `/path/${i}` });
+  }
+
+  await Promise.race([handleRequests(requests), timeLimit()]);
+}
+
+await main();
