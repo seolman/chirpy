4f067de9546080f9fe6e502718359ad9a54c3809
seolman <tjfehdgns@gmail.com>
2025-07-02 23:42:19 +0900

    ts 4-4

--- src/api/chirp.ts
+++ src/api/chirp.ts
@@ -1,21 +1,37 @@
 import { Request, Response } from "express";
 
 export async function handlerValidateChrip(req: Request, res: Response) {
-  type params = {
+  type parameters = {
     body: string;
   };
 
-  let parsedBody: params = req.body;
+  const param: parameters = req.body;
 
   const maxChirpLength = 140;
-  if (parsedBody.body.length > maxChirpLength) {
+  if (param.body.length > maxChirpLength) {
     res.header("Content-Type", "application/json");
     res.status(400).send(JSON.stringify({ error: "Chirp is too long" }));
     res.end();
     return;
   }
 
+  const badWords = [
+    "kerfuffle",
+    "sharbert",
+    "fornax",
+  ];
+  const cleanedBody = param.body
+    .split(" ")
+    .map((word) => {
+      const lower = word.toLocaleLowerCase();
+      if (badWords.includes(lower)) {
+        return "****";
+      }
+      return word
+    })
+    .join(" ");
+
   res.set("Content-Type", "application/json");
-  res.status(200).send(JSON.stringify({ valid: true }));
+  res.status(200).send(JSON.stringify({ cleanedBody }));
   res.end();
 }
--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,11 @@ const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
+app.use(express.json());
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/api/healthz", handlerReadiness);
-app.post("/api/validate_chirp", express.json(), handlerValidateChrip);
+app.post("/api/validate_chirp", handlerValidateChrip);
 
 app.get("/admin/metrics", handlerAdminMetrics);
 app.post("/admin/reset", handlerAdminReset);
