306e79edb717de10181b5f2428bdd74afe40095a
seolman <tjfehdgns@gmail.com>
2025-07-02 12:31:47 +0900

    ts 2-2 config

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { middlewareLogResponses } from "./middleware/log.js";
+import { middlewareMetricsInc } from "./middleware/metrics.js";
+import { handlerMetrics, handlerReset } from "./api/metrics.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
-app.use("/app", express.static("./src/app"));
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerMetrics);
+app.get("/reset", handlerReset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}/`);
---
+++ src/middleware/metrics.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+import { config } from "../config.js";
+
+export function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+  config.fileserverHits += 1;
+  next();
+}
+
